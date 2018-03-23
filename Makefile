a3b0e6ca2229ea39b78af5b08a4bc0c18a5153d1
Uwe Jugel <uwe.jugel@gmail.com>
2018-03-23 22:48:21 +0100

    replace modules

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 .PHONY: test docker-demos
 
+PRJ_MAIN := widdy.demos
+
 include project.mk
 
 test: $(TP_FILES)
-	$(PYTHON) -m widdy.demos --help >/dev/null
+	$(PYTHON) -m $(PRJ_MAIN) --help >/dev/null
 
 docker-test:
 	docker run -it python:$(PY) bash -i -c 'pip install widdy; widdy all; true'
--- project.mk
+++ project.mk
@@ -20,18 +20,21 @@
 #
 # Notes
 # -----
-# All _lower_case vars are internal vars not supposed to be overwritten
+# - All `_lower_case` vars are internal vars not supposed to be overwritten.
+# - Try not to change this file to it your project's needs. Try to handle
+#   all custom building in your main `Makefile`.
 #
 
-.PHONY: all test base-test clean install publish test-publish sign docker-test docker-base-test clone
+.PHONY: all test base-test clean install publish test-publish sign docker-test docker-base-test clone build
 
 # The default project name is the name of the current dir
-PRJ      := $(shell basename $(CURDIR))
+PRJ       := $(shell basename $(CURDIR))
+PRJ_MAIN  := $(PRJ).$(PRJ)
 # All code should reside in another subdir with that name of the project
 PRJ_FILES := setup.py setup.cfg project.cfg project.mk Makefile LICENSE.txt README.md
-TP_FILES := $(PRJ) $(PRJ_FILES)
+TP_FILES  := $(PRJ) $(PRJ_FILES)
 # All code is assumed to be written for Py3, for Py2 support we need to transpile it
-DIST     := transpiled/dist
+DIST      := transpiled/dist
 
 # you can override, e.g., PY=2m to test other python versions
 PY          = 3
@@ -64,6 +67,8 @@ install: $(TP_FILES)
 	# to the code in this repo for quick and easy local development.
 	pip install --user -e .
 
+build: transpiled
+
 transpiled: $(TP_FILES)
 	# copy all code to transpiled, try to convert it to Py2, and build the dist there
 	mkdir -p transpiled
@@ -94,16 +99,47 @@ docker-base-test:
 	# in a clean environment. Safest bet is to use docker!
 	docker run -it python:$(PY) bash -i -c 'pip install $(PRJ); $(IMPORT_TEST); $(CLI_TEST)'
 
-_prj_path  := $(PREFIX)/$(NEW_PRJ)
-_prj_files := $(patsubst %,$(_prj_path)/%,$(PRJ_FILES))
+# Project Clone Target
+# --------------------
+# The `clone` target copies all required files to a new dir and will setup a
+# new python project for you in which you can use the same build features that
+# `project.mk` provides for the current project.
+#
+_prj_path     := $(PREFIX)/$(NEW_PRJ)
+_prj_main     := $(NEW_PRJ).$(NEW_PRJ)
+_prj_package  := $(PREFIX)/$(NEW_PRJ)/$(NEW_PRJ)
+_prj_files    := $(patsubst %,$(_prj_path)/%,$(PRJ_FILES))
+_unsafe_clone := false
+_diff         := 2> /dev/null diff --color
+_expr_eq      := \([ ]*=[ ]*\).*
+_expr_assig   := \(-m[ ]*\|:=[ ]*\|=[ ]*\)
+_expr_sub     := $(PRJ)[a-z\.]\+
+_prj_test     := $(_prj_package)/test_$(NEW_PRJ).py
+_prj_test_def := def test_$(NEW_PRJ)(): pass
+_clone_files  := $(PRJ_FILES) .gitignore
 clone:
-	test -n "$(NEW_PRJ)" -a -n "$(PREFIX)"
-	! test -e $(_prj_path)  # target project path must not exist
-	mkdir -p $(_prj_path)
-	cp $(PRJ_FILES) $(_prj_path)
-	# replace all refs to current project with new project
-	sed -i 's#$(PRJ)#$(NEW_PRJ)#g' $(_prj_files)
-	diff --color $(_prj_path) . 2> /dev/null || true  # compare the copied files to the source files
+	test -n "$(NEW_PRJ)" -a -n "$(PREFIX)"       # ensure that new project name and prefix are set
+	! test -e $(_prj_path) || $(_unsafe_clone)   # target project path must not exist
+	mkdir -p $(_prj_package)                     # create project and package path
+	cp $(_clone_files) $(_prj_path)              # copy all project files
+	sed -i 's#main$(_expr_eq)#main\1$(_prj_main)#g'        $(_prj_path)/project.cfg  # replace main module
+	sed -i 's#$(_expr_assig)$(_expr_sub)#\1$(_prj_main)#g' $(_prj_files)             # replace current main in copied files
+	sed -i 's#$(PRJ)#$(NEW_PRJ)#g'                         $(_prj_files)             # replace current project in copied files
+	touch $(_prj_package)/__init__.py            # create python package
+	touch $(_prj_package)/$(NEW_PRJ).py          # create main module python package
+	test -e $(_prj_test) || echo '$(_prj_test_def)' > $(_prj_test)  # create a test file
+	$(_diff) . $(_prj_path) || true              # compare the copied files to the source files
+	#-------------------------------------------
+	# Cloned $(PRJ) to $(_prj_path)!            
+	# If all went well you can now build it     
+	#-------------------------------------------
+	@echo cd $(_prj_path)                        
+	@echo make                                   
+	@echo make build                             
+	# -------------------------------------------
+
+merge: _unsafe_clone=true
+merge: clone
 
 # docker-test also runs basic import and run test
 docker-test: docker-base-test
