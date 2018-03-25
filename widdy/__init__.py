95932affddfe524c7201f0da60a07aef92c28c50
Uwe Jugel <uwe.jugel@gmail.com>
2018-03-25 16:57:55 +0200

    fix main

--- widdy/__init__.py
+++ widdy/__init__.py
@@ -1,4 +1,5 @@
+# flake8: noqa: F401
 # expose all widgets and important types in main module
-from widdy.widgets import App, Frame, Handlers, Header, KeyFunc, LineBox, Menu, MenuItem, Pal, Text  # noqa
-from widdy.styles import Style                                                                       # noqa
-
+from widdy.widgets import App, Frame, Handlers, Header, KeyFunc, LineBox, Menu, MenuItem, Pal, Text
+from widdy.styles import Style
+from widdy.demos import main
